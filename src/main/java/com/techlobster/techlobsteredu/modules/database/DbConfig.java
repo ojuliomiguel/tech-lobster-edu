ae663fe7b0b8d13509a9da3329cc0da9bfde6cb6
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-03-19 22:44:07 -0300

    feat: creating DbConfig class [WIP]

---
+++ src/main/java/com/techlobster/techlobsteredu/modules/database/DbConfig.java
@@ -0,0 +1,26 @@
+package com.techlobster.techlobsteredu.modules.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Autowired
+    private Environment env;
+    
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUrl(env.getProperty("user"));
+        dataSource.setUrl(env.getProperty("password"));
+        return dataSource;
+    }
+}
